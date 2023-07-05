3d996684264777a6feef2d15326365b97615702d
bryanchrist <brc4cb@virginia.edu>
2023-07-05 08:41:10 -0400

    training model

--- generate.py
+++ generate.py
@@ -79,20 +79,70 @@ sys.stdin = sys.__stdin__
 model = PeftModel.from_pretrained(model, adapter_path)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-prompt = "Write a grade 4 Multiplication question and corresponding equation to solve the problem."
-formatted_prompt = (f"Below is an instruction that describes a task. "
-        f"Write a response that appropriately completes the request.\n\n"
-        f"### Instruction:\n{prompt}\n\n### Response: ")
-inputs = tokenizer.encode(formatted_prompt, return_tensors="pt")
-attention_mask = torch.ones_like(inputs)
-inputs = inputs.to('cuda')
-output = model.generate(inputs=inputs, attention_mask=attention_mask, max_new_tokens = 100)
+for i in range(0, 10):
+    prompt = "Write a grade 4 Multiplication question and corresponding equation to solve the problem."
+    formatted_prompt = (f"Below is an instruction that describes a task. "
+            f"Write a response that appropriately completes the request.\n\n"
+            f"### Instruction:\n{prompt}\n\n### Response: ")
+    inputs = tokenizer.encode(formatted_prompt, return_tensors="pt")
+    attention_mask = torch.ones_like(inputs)
+    inputs = inputs.to('cuda')
+    output = model.generate(inputs=inputs, attention_mask=attention_mask, max_new_tokens = 100)
+    
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    
+    print(generated_text)
+    output_file = "output.txt"  # Specify the path and filename for the output file
+    with open(output_file, "w") as f:
+        f.write(generated_text)
 
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+print("Generated text saved to", output_file)
 
-print(generated_text)
-output_file = "output.txt"  # Specify the path and filename for the output file
-with open(output_file, "w") as f:
-    f.write(generated_text)
+
+adapter_path = "output/checkpoint-250"  # Specify the path to the adapter weights
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+# Patch the built-in input function to return 'y' automatically
+def mock_input(prompt=None):
+    return 'y'
+
+# Patch the input function to use the mock_input function
+builtins.input = mock_input
+
+try:
+    # Attempt to load the model with trust_remote_code=True
+    model = AutoModelForCausalLM.from_pretrained(
+        model_path,
+       # max_memory=max_memory,
+        torch_dtype=torch.bfloat16,
+        config=AutoConfig.from_pretrained(model_path, trust_remote_code=True)
+    )
+except EOFError:
+    # If an EOFError occurs, provide the expected input ('y')
+    pass
+
+# Restore stdin
+sys.stdin = sys.__stdin__
+
+# Load the adapter weights
+model = PeftModel.from_pretrained(model, adapter_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+for i in range(0, 10):
+    prompt = "Write a grade 4 Multiplication question and corresponding equation to solve the problem."
+    formatted_prompt = (f"Below is an instruction that describes a task. "
+            f"Write a response that appropriately completes the request.\n\n"
+            f"### Instruction:\n{prompt}\n\n### Response: ")
+    inputs = tokenizer.encode(formatted_prompt, return_tensors="pt")
+    attention_mask = torch.ones_like(inputs)
+    inputs = inputs.to('cuda')
+    output = model.generate(inputs=inputs, attention_mask=attention_mask, max_new_tokens = 100)
+    
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    
+    print(generated_text)
+    output_file = "output.txt"  # Specify the path and filename for the output file
+    with open(output_file, "w") as f:
+        f.write(generated_text)
 
 print("Generated text saved to", output_file)
